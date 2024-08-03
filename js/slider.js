fde85a0afe30e81a44d3d97cb99b3844f75a0524
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-08-03 13:41:09 +0600

    slider implemented

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,8 @@
-var $element = $('.authority-button-wrapper');
+var $elementAuthorityBtn = $('.authority-button-wrapper');
+
+var $elementTeachersBtn = $('.teachers-button-wrapper');
+
+var $elementGalleryBtn = $('.gallery-button-wrapper');
 
 $(document).ready(function () {
   $('.authority__cards').slick({
@@ -9,7 +13,35 @@ $(document).ready(function () {
     variableWidth: true,
     arrows: true,
     dots: false,
-    appendArrows: $element,
+    appendArrows: $elementAuthorityBtn,
+    centerMode: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.teachers__cards').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    arrows: true,
+    dots: false,
+    appendArrows: $elementTeachersBtn,
+    centerMode: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.gallery__images').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    arrows: true,
+    dots: false,
+    appendArrows: $elementGalleryBtn,
     centerMode: true,
   });
 });
